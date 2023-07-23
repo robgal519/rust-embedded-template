7a694ae67b30b9ef0097bc78a84e43c83f3b4b8d
Robert Gałat <robgal519@gmail.com>
2023-07-23 10:14:35 +0000

    update

--- example-project/src/main.rs
+++ example-project/src/main.rs
@@ -2,20 +2,39 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use defmt::{info, unwrap};
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_time::{Duration, Timer};
+use embassy_nrf::gpio::{AnyPin, Input, Level, Output, OutputDrive, Pin as _, Pull};
 use {defmt_rtt as _, panic_probe as _};
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_nrf::init(Default::default());
-    let mut led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
-
+#[embassy_executor::task(pool_size = 4)]
+async fn button_task(n: usize, mut led: Output<'static, AnyPin>, mut btn: Input<'static, AnyPin>) {
     loop {
-        led.set_high();
-        Timer::after(Duration::from_millis(300)).await;
+        btn.wait_for_low().await;
         led.set_low();
-        Timer::after(Duration::from_millis(300)).await;
+        info!("Button {:?} pressed!", n);
+        btn.wait_for_high().await;
+        led.set_high();
+        info!("Button {:?} released!", n);
     }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+    info!("Starting!");
+
+    let btn1 = Input::new(p.P0_11.degrade(), Pull::Up);
+    let btn2 = Input::new(p.P0_12.degrade(), Pull::Up);
+    let btn3 = Input::new(p.P0_24.degrade(), Pull::Up);
+    let btn4 = Input::new(p.P0_25.degrade(), Pull::Up);
+
+    let led1 = Output::new(p.P0_13.degrade(), Level::High, OutputDrive::Standard);
+    let led2 = Output::new(p.P0_14.degrade(), Level::High, OutputDrive::Standard);
+    let led3 = Output::new(p.P0_15.degrade(), Level::High, OutputDrive::Standard);
+    let led4 = Output::new(p.P0_16.degrade(), Level::High, OutputDrive::Standard);
+    unwrap!(spawner.spawn(button_task(1, led1, btn1)));
+    unwrap!(spawner.spawn(button_task(2, led2, btn2)));
+    unwrap!(spawner.spawn(button_task(3, led3, btn3)));
+    unwrap!(spawner.spawn(button_task(4, led4, btn4)));
 }
